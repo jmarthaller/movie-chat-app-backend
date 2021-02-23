f5d7ea141123cc848800bcbf51bedb11adb64409
Jacob Marthaller <https://github.com/jmarthaller>
2021-02-23 16:15:40 -0500

    model structuring

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+    has_secure_password
+
+    has_many :reviews
+    has_many :movies, through: :reviews
+
+    has_many :followed_users, foreign_key: :follower_id, class_name: 'Friendship'
+    has_many :followees, through: :followed_users
+
+    has_many :following_users, foreign_key: :followee_id, class_name: 'Friendship'
+    has_many :followers, through: :following_users
+
 end
