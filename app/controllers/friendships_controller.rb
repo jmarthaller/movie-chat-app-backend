4407cc41e4fe3fb485764d05706318b72af3dfb0
Jacob Marthaller <https://github.com/jmarthaller>
2021-03-02 12:19:48 -0500

    friendship backend

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -2,5 +2,5 @@ class FriendshipsController < ApplicationController
     def create
         Friendship.create(follower_id: @current_user.id, followee_id: params[:followee_id])
         render json: @current_user
-      end
+    end
 end
