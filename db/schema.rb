f9b564f60a66a2bfe8675a0222308e3eb1e0e2b6
Jacob Marthaller <https://github.com/jmarthaller>
2021-03-04 14:17:06 -0500

    updated overview to text

--- db/schema.rb
+++ db/schema.rb
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 2021_02_23_203515) do
     t.string "genre"
     t.string "tagline"
     t.integer "release_year"
-    t.string "overview"
+    t.text "overview"
     t.integer "rating"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
