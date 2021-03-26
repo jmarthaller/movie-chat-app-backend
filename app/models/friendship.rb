2600d429480e45319b2f47f90ac9ffd58c783c2f
Jacob Marthaller <marthaller.jr@gmail.com>
2021-03-26 07:27:40 -0700

    format models for readability

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -2,7 +2,6 @@ class Friendship < ApplicationRecord
     belongs_to :follower, class_name: "User"  
     belongs_to :followee, class_name: "User" 
 
-
     def follower_object
         self.follower
     end
