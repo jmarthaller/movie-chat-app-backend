39a949d5d4af2453e5c75a5274d4c63bf3bf2909
Jacob Marthaller <marthaller.jr@gmail.com>
2021-03-26 07:24:00 -0700

    reformat rotues.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,4 @@ Rails.application.routes.draw do
   resources :users
   post "/login", to: "users#login"
   post "/signup", to: "users#signup"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
